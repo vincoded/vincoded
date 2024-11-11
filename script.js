5f62192f9b756980c6cab8799a35fec7e7413f5a
vincoded <davinvoeun3@gmail.com>
2024-11-11 13:07:56 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+// Sticky Navigation Menu
+let nav = document.querySelector("nav");
+let scrollBtn = document.querySelector(".scroll-button a");
+
+// Show/hide sticky navigation and scroll button based on scroll position
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 20) {
+    nav.classList.add("sticky");
+    scrollBtn.style.display = "block";
+  } else {
+    nav.classList.remove("sticky");
+    scrollBtn.style.display = "none";
+  }
+};
+
+// Side Navigation Menu
+let body = document.querySelector("body");
+let navBar = document.querySelector(".navbar");
+let menuBtn = document.querySelector(".menu-btn");
+let cancelBtn = document.querySelector(".cancel-btn");
+
+// Open side navigation
+menuBtn.onclick = function () {
+  navBar.classList.add("active");
+  menuBtn.style.opacity = "0";
+  menuBtn.style.pointerEvents = "none";
+  body.style.overflow = "hidden";
+  scrollBtn.style.pointerEvents = "none";
+};
+
+const hideNavMenu = () => {
+  navBar.classList.remove("active");
+  menuBtn.style.opacity = "1";
+  menuBtn.style.pointerEvents = "auto";
+  body.style.overflow = "auto";
+  scrollBtn.style.pointerEvents = "auto";
+};
+
+// Close side navigation
+cancelBtn.onclick = hideNavMenu;
+
+// Close side navigation when a menu link is clicked
+let navLinks = document.querySelectorAll(".menu li a");
+navLinks.forEach((link) => {
+  link.addEventListener("click", hideNavMenu);
+});
+
+// CV Download Script
+document.getElementById("downloadCV").addEventListener("click", function () {
+  // Path to your CV file (can be a local path or URL)
+  const cvFilePath = "path/to/your/cv.pdf"; // Replace with the actual path to your CV file
+  
+  // Create an invisible link element
+  let downloadLink = document.createElement("a");
+  downloadLink.href = cvFilePath;
+  downloadLink.download = "CV.pdf"; // The name that the downloaded file will have
+  
+  // Programmatically trigger a click on the link to start the download
+  downloadLink.click();
+});
